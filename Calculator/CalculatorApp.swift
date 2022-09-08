fc74ab1218b33e624922e869becdb6545a08c2b7
co.abu <anizomxoja@gmail.com>
2022-09-08 12:52:15 +0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Avaz Mukhitdinov on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
